25f3cf3b184da30f50500c98b493b0c81042f347
ToGz <32015332+ToGz@users.noreply.github.com>
2022-05-28 16:19:55 +0200

    created a base implementation of a productAPI

---
+++ ProductAPI/Database/CosmosDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductAPI.Database.Models;
+
+namespace ProductAPI.Database;
+
+public class CosmosDbContext : DbContext
+{
+    public CosmosDbContext(DbContextOptions options) : base(options) {}
+    public DbSet<Product> Products { get; set; }
+}
---
+++ ProductAPI/Queries/ProductQueryEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Cosmos.Linq;
+using Microsoft.EntityFrameworkCore;
+using ProductAPI.Database;
+
+namespace ProductAPI.Queries;
+
+public static class ProductQueryEndpoint
+{
+    public static void MapProductQueryEndpoint(this WebApplication app)
+    {
+        app.MapGet("/product", async (CosmosDbContext context, string id) =>
+        {
+            var product = await context.Products
+                .Where(prod => prod.Id.Equals(id))
+                .SingleAsync();
+            
+            return product;
+        }).WithName("GetProduct");
+    }
+}
