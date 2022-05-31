b8fdc41b2a30de1e580284ba2177e5ac4e503401
ToG <to.gadzik@gmail.com>
2022-05-31 20:14:53 +0200

    created a ProductQueryRepository

--- ProductsAPI/Queries/ProductQueryEndpoint.cs
+++ ProductsAPI/Queries/ProductQueryEndpoint.cs
@@ -1,19 +1,38 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ProductsAPI.Database;
+using ProductsAPI.Database.Models;
 
 namespace ProductsAPI.Queries;
 
 public static class ProductQueryEndpoint
 {
+    /*
+     TODO: We need two endpoints here:
+        GetByID
+        Search for product by name / code 
+    */
     public static void MapProductQueryEndpoint(this WebApplication app)
     {
-        app.MapGet("/product", async (CosmosDbContext context, string id) =>
+        app.MapGet("/product", async (string query) =>
         {
-            var product = await context.Products
-                .Where(prod => prod.Id.Equals(id))
-                .SingleAsync();
             
-            return product;
         }).WithName("GetProduct");
+    }
+    
+    public static void MapProductIdQueryEndpoint(this WebApplication app)
+    {
+        app.MapGet("/product/{id}", async (CosmosDbContext context, string id) =>
+        {
+            
+        }).WithName("GetProductById");
+    }
+
+    private static async Task<Product> GetProductsFromDatabase(CosmosDbContext context, string id)
+    {
+        var product = await context.Products
+            .Where(prod => prod.Id.Equals(id))
+            .SingleAsync();
+            
+        return product;
     }
 }
